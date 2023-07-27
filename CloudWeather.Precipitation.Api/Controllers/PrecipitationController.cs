d67b7b6ebf34657657eb8ff8ad4656806a5f7a24
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-27 12:20:51 +0300

    Added a dependency injection to for the database to yhe applciation

--- CloudWeather.Precipitation.Api/Controllers/PrecipitationController.cs
+++ CloudWeather.Precipitation.Api/Controllers/PrecipitationController.cs
@@ -1,3 +1,4 @@
+using CloudWeather.Precipitation.Api.DataAccess;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CloudWeather.Precipitation.Api.Controllers;
@@ -6,6 +7,13 @@ namespace CloudWeather.Precipitation.Api.Controllers;
 [Route("api/[controller]")]
 public class PrecipitationController : ControllerBase
 {
+    private readonly PrecipDbContext _context;
+
+    public PrecipitationController(PrecipDbContext context)
+    {
+        _context = context;
+    }
+
     [HttpGet("{zip}")]
     public IActionResult GetObservation(string zip, [FromQuery] int? days)
     {
