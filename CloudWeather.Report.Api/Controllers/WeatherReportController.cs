6c153a490ff89c24d455885cdbb6521cc0b35c97
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-27 15:54:49 +0300

    Defined the endpoint to get the weather report by zip code

--- CloudWeather.Report.Api/Controllers/WeatherReportController.cs
+++ CloudWeather.Report.Api/Controllers/WeatherReportController.cs
@@ -1,3 +1,4 @@
+using CloudWeather.Report.Api.BusinessLogic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CloudWeather.Report.Api.Controllers;
@@ -6,5 +7,16 @@ namespace CloudWeather.Report.Api.Controllers;
 [Route("api/[controller]")]
 public class WeatherReportController : ControllerBase
 {
+    [HttpGet("weather-report/{zip}")]
+    public async Task<IActionResult> GetWeatherReportByZipCode(string zip, [FromQuery] int? days, IWeatherReportAggregator weatherAgg)
+    {
+        if (days is null)
+        {
+            return BadRequest("Please provide the 'days' query parameter");
+        }
 
+        var report = await weatherAgg.BuildReport(zip, days.Value);
+
+        return Ok(report);
+    }
 }
