3d0c4c831088956835baf9afdb895062499c5d14
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-27 12:00:14 +0300

    Removed the data type from the route variable

--- CloudWeather.Precipitation.Api/Controllers/PrecipitationController.cs
+++ CloudWeather.Precipitation.Api/Controllers/PrecipitationController.cs
@@ -6,7 +6,7 @@ namespace CloudWeather.Precipitation.Api.Controllers;
 [Route("api/[controller]")]
 public class PrecipitationController : ControllerBase
 {
-    [HttpGet("{zip:string}")]
+    [HttpGet("{zip}")]
     public IActionResult GetObservation(string zip)
     {
         return Ok(zip);
