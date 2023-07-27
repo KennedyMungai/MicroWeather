10bc1beed50a77b586023a8fe87e2a637b079d60
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-27 12:12:31 +0300

    Added some data annotation on the Id property of the data

--- CloudWeather.Precipitation.Api/DataAccess/Precipitation.cs
+++ CloudWeather.Precipitation.Api/DataAccess/Precipitation.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CloudWeather.Precipitation.Api.DataAccess;
 
 public class Precipitation
 {
+    [Key]
     public Guid Id { get; set; }
     public DateTime CreatedOn { get; set; }
     public decimal AmountInches { get; set; }
