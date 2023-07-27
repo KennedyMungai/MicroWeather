3a46011686574fee28ebe5a9b0f1080f13755a40
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-27 14:30:30 +0300

    Defined migrations for the updated model file name

--- CloudWeather.Temperature.Api/DataAccess/Migrations/20230727112948_UpdatedModelName.Designer.cs
+++ CloudWeather.Temperature.Api/DataAccess/Migrations/20230727112948_UpdatedModelName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CloudWeather.Temperature.Api.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CloudWeather.Temperature.Api.DataAccess.Migrations
 {
     [DbContext(typeof(TempDbContext))]
-    partial class TempDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230727112948_UpdatedModelName")]
+    partial class UpdatedModelName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
