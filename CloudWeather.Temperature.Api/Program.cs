df759d11107bc94ed22b1a89917d9b4608dbbd3e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-27 13:45:11 +0300

    Added the DbCOntext service for the microservice

--- CloudWeather.Temperature.Api/Program.cs
+++ CloudWeather.Temperature.Api/Program.cs
@@ -1,3 +1,6 @@
+using CloudWeather.Temperature.Api.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TempDbContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("TempDb"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("TempDb"))
+    );
+});
 
 var app = builder.Build();
 
