7caf05dae0a0e56b89c73261e0f102a4a94ea27f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-28 09:59:07 +0300

    Defined the Precipitation model

--- CloudWeather.DataLoader.Console/Models/PrecipitationModel.cs
+++ CloudWeather.DataLoader.Console/Models/PrecipitationModel.cs
@@ -2,5 +2,8 @@ namespace CloudWeather.DataLoader.Console.Models;
 
 public class PrecipitationModel
 {
-
+    public DateTime CreatedOn { get; set; }
+    public decimal AmountByInches { get; set; }
+    public string WeatherType { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
 }
