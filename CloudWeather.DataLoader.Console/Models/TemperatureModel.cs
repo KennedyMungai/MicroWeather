850e42581993406eb7e71fe5105c328392a83c02
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-28 10:00:13 +0300

    Defined the temperature model\

--- CloudWeather.DataLoader.Console/Models/TemperatureModel.cs
+++ CloudWeather.DataLoader.Console/Models/TemperatureModel.cs
@@ -2,5 +2,8 @@ namespace CloudWeather.DataLoader.Console.Models;
 
 public class TemperatureModel
 {
-
+    public DateTime CreatedOn { get; set; }
+    public decimal TempHighF { get; set; }
+    public decimal TempLowF { get; set; }
+    public string ZipCode { get; set; } = string.Empty;
 }
