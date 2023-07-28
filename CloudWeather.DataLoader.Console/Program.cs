a8b1f8c44b51fee87982ca65fc1fa365ce61454f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-28 10:15:24 +0300

    Fetched some values from the appsettings json file

--- CloudWeather.DataLoader.Console/Program.cs
+++ CloudWeather.DataLoader.Console/Program.cs
@@ -4,3 +4,13 @@ IConfiguration configuration = new ConfigurationBuilder()
                                 .AddJsonFile("appsettings.json")
                                 .AddEnvironmentVariables()
                                 .Build();
+
+var servicesConfig = configuration.GetSection("Services");
+
+var tempServiceConfig = servicesConfig.GetSection("Temperature");
+var tempServiceHost = tempServiceConfig["Host"];
+var tempServicePort = tempServiceConfig["Port"];
+
+var precipServiceConfig = servicesConfig.GetSection("Precipitation");
+var precipServiceHost = precipServiceConfig["Host"];
+var precipServicePort = precipServiceConfig["Port"];
